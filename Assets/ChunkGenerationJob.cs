59497261758530276250ac7c6a7bac0dbf533a1a
KingPengu <rutger2003@gmail.com>
2024-10-22 09:34:51 +0200

    Added noise generation shaders, updated worldgenerator

--- Assets/ChunkGenerationJob.cs
+++ Assets/ChunkGenerationJob.cs
@@ -22,24 +22,17 @@ public struct ChunkGenerationJob : IJob
     int totalChunkSize;
     int totalChunkSize2;
 
-    //public NativeArray<int> axisCols;
-    //public NativeArray<int> faceCols;
-    //public NativeArray<int> faceRows;
-
     int numVerts;
 
     public void Execute()
     {
-
-        //verts = new NativeList<float3>();
-        //tris = new NativeList<int>();
-        //normals = new NativeList<float3>();
 
         totalChunkSize = chunkSize + 2;
         totalChunkSize2 = totalChunkSize * totalChunkSize;
 
         NativeArray<int> axisCols = new NativeArray<int>(totalChunkSize * totalChunkSize * 3, Allocator.Temp);
         NativeArray<int> faceCols = new NativeArray<int>(totalChunkSize * totalChunkSize * 3 * 2, Allocator.Temp);
+        NativeArray<int> faceRows = new NativeArray<int>(totalChunkSize * totalChunkSize * 3 * 2, Allocator.Temp);
 
         numVerts = 0;
 
@@ -64,8 +57,6 @@ public struct ChunkGenerationJob : IJob
                 }
             }
         }
-
-        //inputMap.Dispose();
 
         //setting up face columns by bitshifting and comparing in both directions for each axis
         for (int axis = 0; axis < 3; axis++)
@@ -102,37 +93,70 @@ public struct ChunkGenerationJob : IJob
                         //clear least significant set bit
                         col &= col - 1;
 
-                        //faceRows[y + (z * totalChunkSize) + (totalChunkSize2 * axis)] |= 1 << x;
+                        faceRows[y + (z * totalChunkSize) + (totalChunkSize2 * axis)] |= 1 << x;
 
                         //regular ole meshing
-                        switch (axis)
+                        //switch (axis)
+                        //{
+                        //    //x+
+                        //    case 0:
+                        //        faceConstructor(y, x, z, 1);
+                        //        break;
+                        //    //x-
+                        //    case 1:
+                        //        faceConstructor(y, x, z, 0);
+                        //        break;
+                        //    //y+
+                        //    case 2:
+                        //        faceConstructor(x, y, z, 3);
+                        //        break;
+                        //    //y-
+                        //    case 3:
+                        //        faceConstructor(x, y, z, 2);
+                        //        break;
+                        //    //z+
+                        //    case 4:
+                        //        faceConstructor(x, z, y, 5);
+                        //        break;
+                        //    //z-
+                        //    case 5:
+                        //        faceConstructor(x, z, y, 4);
+                        //        break;
+                        //}
+
+                    }
+                }
+            }
+        }
+
+        for (int axis = 0; axis < 6; axis++)
+        {
+            for (int height = 1; height < chunkSize; height++)
+            {
+                for (int row = 1; row < chunkSize; row++)
+                {
+
+                    int currentRow = faceRows[row + (height * totalChunkSize) + (axis * totalChunkSize2)];
+
+                    int y = 0;
+                    while (y < totalChunkSize)
+                    {
+                        y = math.tzcnt(currentRow);
+                        if (y >= totalChunkSize)
                         {
-                            //x+
-                            case 0:
-                                faceConstructor(y, x, z, 1);
-                                break;
-                            //x-
-                            case 1:
-                                faceConstructor(y, x, z, 0);
-                                break;
-                            //y+
-                            case 2:
-                                faceConstructor(x, y, z, 3);
-                                break;
-                            //y-
-                            case 3:
-                                faceConstructor(x, y, z, 2);
-                                break;
-                            //z+
-                            case 4:
-                                faceConstructor(x, z, y, 5);
-                                break;
-                            //z-
-                            case 5:
-                                faceConstructor(x, z, y, 4);
-                                break;
+                            continue;
                         }
 
+                        //to get the trailing ones, shift to the right by the amount of trailing zeros, then flip and get trailing zeros of new number.
+                        int h = math.tzcnt(~(currentRow >> y));
+
+                        //EXPAND HERE
+
+                        faceConstructor(row, height, y, axis, h);
+
+                        //shifting to the right and left to remove the bits we just encountered
+                        currentRow = currentRow >> y + h;
+                        currentRow = currentRow << y + h;
                     }
                 }
             }
@@ -140,7 +164,105 @@ public struct ChunkGenerationJob : IJob
 
         axisCols.Dispose();
         faceCols.Dispose();
+        faceRows.Dispose();
 
+    }
+
+    void faceConstructor(int x, int y, int z, int dir, int height)
+    {
+        switch (dir)
+        {
+            //x+
+            case 1:
+                verts.Add(new Vector3(x + 1, y + height, z));
+                verts.Add(new Vector3(x + 1, y + 1 + height, z));
+                verts.Add(new Vector3(x + 1, y + height, z + height));
+                verts.Add(new Vector3(x + 1, y + 1 + height, z + height));
+
+                numVerts += 4;
+
+                normals.Add(new Vector3(1, 0, 0)); normals.Add(new Vector3(1, 0, 0));
+                normals.Add(new Vector3(1, 0, 0)); normals.Add(new Vector3(1, 0, 0));
+
+                tris.Add(numVerts - 4); tris.Add(numVerts - 3); tris.Add(numVerts - 2);
+                tris.Add(numVerts - 1); tris.Add(numVerts - 2); tris.Add(numVerts - 3);
+                break;
+            //x-
+            case 0:
+                verts.Add(new Vector3(x, y, z));
+                verts.Add(new Vector3(x, y + 1, z));
+                verts.Add(new Vector3(x, y, z + height));
+                verts.Add(new Vector3(x, y + 1, z + height));
+
+                numVerts += 4;
+
+                normals.Add(new Vector3(-1, 0, 0)); normals.Add(new Vector3(-1, 0, 0));
+                normals.Add(new Vector3(-1, 0, 0)); normals.Add(new Vector3(-1, 0, 0));
+
+                tris.Add(numVerts - 4); tris.Add(numVerts - 2); tris.Add(numVerts - 3);
+                tris.Add(numVerts - 1); tris.Add(numVerts - 3); tris.Add(numVerts - 2);
+                break;
+            //y+
+            case 3:
+                verts.Add(new Vector3(x, y + 1, z));
+                verts.Add(new Vector3(x + 1, y + 1, z));
+                verts.Add(new Vector3(x, y + 1, z + height));
+                verts.Add(new Vector3(x + 1, y + 1, z + height));
+
+                numVerts += 4;
+
+                normals.Add(new Vector3(0, 1, 0)); normals.Add(new Vector3(0, 1, 0));
+                normals.Add(new Vector3(0, 1, 0)); normals.Add(new Vector3(0, 1, 0));
+
+                tris.Add(numVerts - 4); tris.Add(numVerts - 2); tris.Add(numVerts - 3);
+                tris.Add(numVerts - 3); tris.Add(numVerts - 2); tris.Add(numVerts - 1);
+                break;
+            //y-
+            case 2:
+                verts.Add(new Vector3(x, y, z));
+                verts.Add(new Vector3(x + 1, y, z));
+                verts.Add(new Vector3(x, y, z + height));
+                verts.Add(new Vector3(x + 1, y, z + height));
+
+                numVerts += 4;
+
+                normals.Add(new Vector3(0, -1, 0)); normals.Add(new Vector3(0, -1, 0));
+                normals.Add(new Vector3(0, -1, 0)); normals.Add(new Vector3(0, -1, 0));
+
+                tris.Add(numVerts - 4); tris.Add(numVerts - 3); tris.Add(numVerts - 2);
+                tris.Add(numVerts - 2); tris.Add(numVerts - 3); tris.Add(numVerts - 1);
+                break;
+            //z+
+            case 5:
+                verts.Add(new Vector3(x, y, z + 1));
+                verts.Add(new Vector3(x + 1, y, z + 1));
+                verts.Add(new Vector3(x, y + height, z + 1));
+                verts.Add(new Vector3(x + 1, y + height, z + 1));
+
+                numVerts += 4;
+
+                normals.Add(new Vector3(0, 0, 1)); normals.Add(new Vector3(0, 0, 1));
+                normals.Add(new Vector3(0, 0, 1)); normals.Add(new Vector3(0, 0, 1));
+
+                tris.Add(numVerts - 2); tris.Add(numVerts - 4); tris.Add(numVerts - 3);
+                tris.Add(numVerts - 1); tris.Add(numVerts - 2); tris.Add(numVerts - 3);
+                break;
+            //z-
+            case 4:
+                verts.Add(new Vector3(x, y, z));
+                verts.Add(new Vector3(x + 1, y, z));
+                verts.Add(new Vector3(x, y + height, z));
+                verts.Add(new Vector3(x + 1, y + height, z));
+
+                numVerts += 4;
+
+                normals.Add(new Vector3(0, 0, -1)); normals.Add(new Vector3(0, 0, -1));
+                normals.Add(new Vector3(0, 0, -1)); normals.Add(new Vector3(0, 0, -1));
+
+                tris.Add(numVerts - 2); tris.Add(numVerts - 3); tris.Add(numVerts - 4);
+                tris.Add(numVerts - 1); tris.Add(numVerts - 3); tris.Add(numVerts - 2);
+                break;
+        }
     }
 
     void faceConstructor(int x, int y, int z, int dir)
--- Assets/WorldGenerator.cs
+++ Assets/WorldGenerator.cs
@@ -9,6 +9,35 @@ using UnityEngine.Jobs;
 using Unity.Collections;
 using System.Diagnostics;
 using NUnit.Framework.Constraints;
+using static UnityEditor.PlayerSettings;
+
+public class ChunkJobData
+{
+    public JobHandle JobHandle;
+
+    public NativeList<float3> verts;
+    public NativeList<int> tris;
+    public NativeList<float3> normals;
+
+    public ChunkJobData()
+    {
+        verts = new NativeList<float3>(Allocator.Persistent);
+        tris = new NativeList<int>(Allocator.Persistent);
+        normals = new NativeList<float3>(Allocator.Persistent);
+    }
+
+    public void AttachJob(JobHandle inJobHandle)
+    {
+        JobHandle = inJobHandle;
+    }
+
+    public void DisposeAll()
+    {
+        verts.Dispose();
+        tris.Dispose();
+        normals.Dispose();
+    }
+}
 
 public class WorldGenerator : MonoBehaviour
 {
@@ -19,13 +48,13 @@ public class WorldGenerator : MonoBehaviour
 
     [SerializeField] int viewDistance;
 
-    ChunkGenerator[,,] chunks;
+    //ChunkGenerator[,,] chunks;
 
     [SerializeField] int chunkSize;
 
     GameObject[,,] chunkObjects;
 
-    Vector3 playerPosition;
+    int3 mapOffset;
 
     List<int3> chunksToUpdate;
 
@@ -36,6 +65,7 @@ public class WorldGenerator : MonoBehaviour
     JobHandle[,,] noiseJobHandles;
     JobHandle[,,] chunkJobHandles;
 
+    //List<ChunkJobData> chunkJobs;
     List<JobHandle> noiseJobHandlesList;
     List<JobHandle> chunkJobHandlesList;
     List<int3> noiseJobHandlesPos;
@@ -47,27 +77,38 @@ public class WorldGenerator : MonoBehaviour
     NativeList<int>[,,] tris;
     NativeList<float3>[,,] normals;
 
-    int time;
+
+    [Header("generation speed settings")]
 
     [SerializeField] int maxNoiseUpdates;
     [SerializeField] int maxChunkUpdates;
     [SerializeField] int maxMeshConstructions;
 
-    //private void Update()
-    //{
-    //    if (time == 10) Generate();
 
-    //    time++;
-    //}
+    [Header("noise settings")]
+
+    [SerializeField] ComputeShader cShader;
+    RenderTexture texture;
+
+    int noiseSize;
+
+    [SerializeField] float scale;
+    [SerializeField] int octaves;
+    [SerializeField] float scaleStep;
+    [SerializeField] float strengthStep;
+    [SerializeField] float strength;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        mapOffset = new int3((int)player.transform.position.x, (int)player.transform.position.y, (int)player.transform.position.z);
 
         chunkObjects = new GameObject[viewDistance, viewDistance, viewDistance];
-        chunks = new ChunkGenerator[viewDistance, viewDistance, viewDistance];
         noiseJobHandles = new JobHandle[viewDistance, viewDistance, viewDistance];
         chunkJobHandles = new JobHandle[viewDistance, viewDistance, viewDistance];
+
+        //chunkJobs = new List<ChunkJobData>();
 
         noiseJobHandlesList = new List<JobHandle>();
         noiseJobHandlesPos = new List<int3>();
@@ -80,7 +121,6 @@ public class WorldGenerator : MonoBehaviour
         tris = new NativeList<int>[viewDistance, viewDistance, viewDistance];
         normals = new NativeList<float3>[viewDistance, viewDistance, viewDistance];
 
-        //chunksToUpdate = new List<ChunkGenerator>();
         chunksToUpdate = new List<int3>();
 
         Stopwatch sw = Stopwatch.StartNew();
@@ -93,10 +133,9 @@ public class WorldGenerator : MonoBehaviour
                 {
                     chunkObjects[x, y, z] = new GameObject("chunk: " + x + ", " + y + ", " + z);
                     chunkObjects[x, y, z].transform.position = new Vector3(x * (chunkSize - 1), y * (chunkSize - 1), z * (chunkSize - 1));
-                    chunks[x, y, z] = chunkObjects[x, y, z].AddComponent<ChunkGenerator>();
-                    chunks[x, y, z].AddComponent<MeshFilter>();
-                    chunks[x, y, z].AddComponent<MeshRenderer>();
-                    chunks[x, y, z].GetComponent<MeshRenderer>().material = tempMat;
+                    chunkObjects[x, y, z].AddComponent<MeshFilter>();
+                    chunkObjects[x, y, z].AddComponent<MeshRenderer>();
+                    chunkObjects[x, y, z].GetComponent<MeshRenderer>().material = tempMat;
 
                     chunksToUpdate.Add(new int3(x, y, z));
                 }
@@ -160,7 +199,7 @@ public class WorldGenerator : MonoBehaviour
         int chunkUpdates = 0;
         int meshConstructions = 0;
 
-        for (int i = chunksToUpdate.Count - 1; i >= 0 && noiseUpdates < maxMeshConstructions; i--)
+        for (int i = chunksToUpdate.Count - 1; i >= 0 && noiseUpdates < maxNoiseUpdates; i--)
         {
             int3 pos = chunksToUpdate[i];
             noiseMaps[pos.x, pos.y, pos.z] = new NativeArray<float>(32 * 32 * 32, Allocator.Persistent);
@@ -173,9 +212,12 @@ public class WorldGenerator : MonoBehaviour
                 noiseValue = noiseMaps[pos.x, pos.y, pos.z]
             };
 
-            //noiseJobHandles[x,y,z] = job.Schedule(32 * 32 * 32, 128);
             noiseJobHandlesList.Add(job.Schedule(32 * 32 * 32, 128));
             noiseJobHandlesPos.Add(new int3(pos.x, pos.y, pos.z));
+
+            //ComputeNoiseSection(chunksToUpdate[i], 4);
+
+
 
             //ah, there you are
             chunksToUpdate.RemoveAt(i);
@@ -184,7 +226,7 @@ public class WorldGenerator : MonoBehaviour
         }
 
 
-        for (int i = noiseJobHandlesList.Count - 1; i >= 0 && chunkUpdates < maxNoiseUpdates; i--)
+        for (int i = noiseJobHandlesList.Count - 1; i >= 0 && chunkUpdates < maxChunkUpdates; i--)
         {
             if (noiseJobHandlesList[i].IsCompleted)
             {
@@ -196,11 +238,14 @@ public class WorldGenerator : MonoBehaviour
                 int3 pos = noiseJobHandlesPos[i];
                 noiseJobHandlesPos.RemoveAt(i);
 
+
+
                 verts[pos.x, pos.y, pos.z] = new NativeList<float3>(Allocator.Persistent);
                 tris[pos.x, pos.y, pos.z] = new NativeList<int>(Allocator.Persistent);
                 normals[pos.x, pos.y, pos.z] = new NativeList<float3>(Allocator.Persistent);
 
-
+                //ChunkJobData currentJob = new ChunkJobData();
+                //chunkJobs.Add(currentJob);
 
                 //NativeArray<float> inputArray = new NativeArray<float>(32 * 32 * 32, Allocator.TempJob);
                 //noiseMaps[pos.x, pos.y, pos.z].CopyTo(inputArray);
@@ -211,22 +256,29 @@ public class WorldGenerator : MonoBehaviour
                     verts = verts[pos.x, pos.y, pos.z],
                     tris = tris[pos.x, pos.y, pos.z],
                     normals = normals[pos.x, pos.y, pos.z],
+
+                    //verts = currentJob.verts,
+                    //tris = currentJob.tris,
+                    //normals = currentJob.normals,
+
                     chunkSize = 30,
 
                 };
 
-                //chunkJobHandles[x, y, z] = job.Schedule();
                 chunkJobHandlesList.Add(job.Schedule());
                 chunkJobHandlesPos.Add(pos);
+
+                //currentJob.AttachJob(job.Schedule());
 
                 chunkUpdates++;
             }
         }
 
-        for (int i = chunkJobHandlesList.Count - 1; i >= 0 && meshConstructions < maxChunkUpdates; i--)
+        for (int i = chunkJobHandlesList.Count - 1; i >= 0 && meshConstructions < maxMeshConstructions; i--)
         {
             if (chunkJobHandlesList[i].IsCompleted)
             {
+                //chunkJobs[i].JobHandle.Complete();
 
                 chunkJobHandlesList[i].Complete();
                 chunkJobHandlesList.RemoveAt(i);
@@ -241,6 +293,11 @@ public class WorldGenerator : MonoBehaviour
                 mesh.SetVertices(verts[pos.x, pos.y, pos.z].AsArray().Reinterpret<Vector3>());
                 mesh.SetTriangles(tris[pos.x, pos.y, pos.z].AsArray().ToArray(), 0);
                 mesh.SetNormals(normals[pos.x, pos.y, pos.z].AsArray());
+
+                //mesh.SetVertices(chunkJobs[i].verts.AsArray().Reinterpret<Vector3>());
+                //mesh.SetTriangles(chunkJobs[i].tris.AsArray().ToArray(), 0);
+                //mesh.SetNormals(chunkJobs[i].normals.AsArray());
+
                 mesh.UploadMeshData(false);
 
                 noiseMaps[pos.x, pos.y, pos.z].Dispose();
@@ -249,12 +306,44 @@ public class WorldGenerator : MonoBehaviour
                 tris[pos.x, pos.y, pos.z].Dispose();
                 normals[pos.x, pos.y, pos.z].Dispose();
 
-                chunks[pos.x, pos.y, pos.z].GetComponent<MeshFilter>().mesh = mesh;
+                //chunkJobs[i].DisposeAll();
+
+                chunkObjects[pos.x, pos.y, pos.z].GetComponent<MeshFilter>().mesh = mesh;
 
                 meshConstructions++;
             }
         }
     }
+
+    void RearrangeChunks(int3 newOffset)
+    {
+        int3 movement = mapOffset - newOffset;
+
+        for (int x = 0; x < viewDistance; x++)
+        {
+            for (int y = 0; y < viewDistance; y++)
+            {
+                for (int z = 0; z < viewDistance; z++)
+                {
+                    chunkObjects[x, y, z] = new GameObject("chunk: " + x + ", " + y + ", " + z);
+                    chunkObjects[x, y, z].transform.position = new Vector3(x * (chunkSize - 1), y * (chunkSize - 1), z * (chunkSize - 1));
+                    chunkObjects[x, y, z].AddComponent<MeshFilter>();
+                    chunkObjects[x, y, z].AddComponent<MeshRenderer>();
+                    chunkObjects[x, y, z].GetComponent<MeshRenderer>().material = tempMat;
+
+                    chunksToUpdate.Add(new int3(x, y, z));
+                }
+            }
+        }
+    }
+
+    //void ComputeNoiseSection(int3 startingPos, int maxSize)
+    //{
+    //    for(int x = 0; x < maxSize; x++)
+    //    {
+    //        if(noiseSe)
+    //    }
+    //}
 
     // Update is called once per frame
     //void Update()
